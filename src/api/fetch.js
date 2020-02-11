29cd1e1e2d5290a191ec2132ae9186429194811f
leida <zhang20084@126.com>
2020-02-11 15:01:31 +0800

    build:升级

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -16,8 +16,7 @@ axios.interceptors.request.use(
     }
     // Cookies.set('authenticationToken','eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJoeWZAYmFuZ3F1LmNvbSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE1NjI0MTgzOTN9.M64dJH7n4wrMgqpv4ABgGeAjnM-3eeijl_NNBa4GeHXRqnkuY8z2ErW30Cm6TZCgg07CXJWwNCsoJDm6FdX6hA');
     //全局加Token
-    config.headers.authorization =
-      "Bearer " + Cookies.get("authenticationToken");
+    config.headers.authorization = "Bearer " + Cookies.get("authenticationToken");
     return config;
   },
   function (error) {
